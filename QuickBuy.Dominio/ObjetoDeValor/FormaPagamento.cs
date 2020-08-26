b3eb5175ebc0b4325354246b3cf1f91fabf5200a
Guilherme Lopes <guiplopes29@gmail.com>
2020-08-26 13:59:05 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,28 @@
+﻿using QuickBuy.Dominio.Enumerados;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool Eboleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool ECartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
